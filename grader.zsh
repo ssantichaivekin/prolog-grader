a1fcd19de6a51603833e1ab304d8c92df40f5e8a
ssantichaivekin <jsantichaivekin@g.hmc.edu>
2018-10-09 23:41:21 -0700

    formatting / add demo

--- grader.zsh
+++ grader.zsh
@@ -1,6 +1,6 @@
 # Requirement checks
 # check for timeout or gtimeout and swipl
-check-requirement() {
+check-requirement () {
     timeoutcmd=''
     requirementsSatisfied='true'
     if type timeout >&- ; then
@@ -23,36 +23,36 @@ check-requirement() {
         requirementsSatisfied='false'
     fi
 
-    result=[[ requirementsSatisfied == 'true' ]]
+    result=[[ ${requirementsSatisfied} = 'true' ]]
     return result
 }
 
 # checks that all drivers have matching solution.
 check-driver-solution () {
-    gradingDir=${1:-'./'}
+    gradingDir=${1:-'.'}
     requirementsSatisfied='true'
     for testDriver in ${gradingDir}/tests/*driver*.pl ; do
         # find the test solution (expected output) by changing the word 
         # driver to solution. the solution should be a text file.
-        testSolution=${testDriver:s/driver/solution:s/pl/txt}
+        testSolution=${testDriver:s/driver/solution/:s/pl/txt/}
         # if the solution does not exist for that test, skip
-        if [[ ! -a testSolution ]] ; then
+        if [[ ! -a ${testSolution} ]] ; then
             echo "${testDriver} has no matching solution"
             echo "Expected file: ${testSolution}"
             requirementsSatisfied='false'
         fi
     done
 
-    result=[[ requirementsSatisfied == 'true' ]]
+    result=[[ ${requirementsSatisfied} = 'true' ]]
     return result
 }
 
 # If requirements are not satisfied, exit.
-if ! check-requirement ; do
+if ! check-requirement ; then
     exit 1
 fi
 # If requirements are not satisfied, exit.
-if ! check-driver-solution ; do
+if ! check-driver-solution $1 ; then
     exit 1
 fi
 
@@ -71,7 +71,7 @@ run-prolog () {
 similar-text () {
     # use awk to format both text files and then
     # use diff to see whether they are equal
-    diff =( awk 'length {$1=$1;print}' $1 ) =( awk 'length {$1=$1;print}' $2 ) 2>&-
+    cmp =( awk 'length {$1=$1;print}' $1 ) =( awk 'length {$1=$1;print}' $2 ) 
 }
 
 run-and-grade-all () {
@@ -82,39 +82,46 @@ run-and-grade-all () {
     # for each problem submission in the current directory
     for submission in ${gradingDir}/*problem*.pl ; do
         # create a folder to store that person's result
-        submissionFolder=${submission:s/driver/}
+        submissionFolder=${submission:s/.pl//}
+        mkdir -p ${submissionFolder}
         # create a summary file
         summaryFile=${submissionFolder}/summary.txt
-        echo 'Grading for' ${summaryFile} ':'
-        echo '#####' 'Grading submission:' ${submission:s/driver/} '#####'
+        echo "Summary for ${submission}" > ${summaryFile}
+        echo '#####' 'Grading submission:' ${submission} '#####'
         # set the counters to 0
         (( countCorrectTest = 0 ))
         (( countTotalTest = 0 ))
         # for each pair of test case driver/solution file in test directory
         for testDriver in ${gradingDir}/tests/*driver*.pl ; do
             # create and write to summary file
-            echo ${testDriver} > ${summaryFile}
+            echo ${testDriver} >> ${summaryFile}
             # find the test solution (expected output) by changing the word 
             # driver to solution. the solution should be a text file.
-            testSolution=${testDriver:s/driver/solution:s/pl/txt}
-            outDest=${testDriver:s/driver/output:s/pl/txt}
-            errDest=${testDriver:s/driver/errors:s/pl/txt}
+            testSolution=${testDriver:s/driver/solution/:s/pl/txt/}
+            outDest=${submissionFolder}/${testDriver:t:s/driver/output/:s/pl/txt/}
+            errDest=${submissionFolder}/${testDriver:t:s/driver/errors/:s/pl/txt/}
             run-prolog ${helperFiles} ${submission} ${testDriver} \
-                            1> ${submissionFolder}/${outDest} \
-                            2> ${submissionFolder}/${errDest}
+                            1> ${outDest} 2> ${errDest}
             # if the submission output is the same as the expected
             # output, we add the score, if not, just add the total
-            if similar-text ${outDest} ${errDest} ; then
+            if similar-text ${outDest} ${testSolution} >/dev/null ; then
                 (( countCorrectTest += 1))
                 (( countTotalTest += 1))
-                echo 'PASS' > ${summaryFile}
+                echo 'PASS' >> ${summaryFile}
             else
                 (( countTotalTest += 1))
-                echo 'FAIL' > ${summaryFile}
+                echo 'FAIL' >> ${summaryFile}
+                echo 'Output :' >> ${summaryFile}
+                cat ${outDest} >> ${summaryFile}
+                echo 'Solution :' >> ${summaryFile}
+                cat ${testSolution} >> ${summaryFile}
             fi
         done
-        echo 'CorrectTests:' ${countCorrectTest} '/' ${countTotalTest} > ${summaryFile}
+        echo '' >> ${summaryFile}
+        echo 'CorrectTests:' ${countCorrectTest} '/' ${countTotalTest} >&1 >> ${summaryFile} 
     done
 }
+
+run-and-grade-all $1
 
 
